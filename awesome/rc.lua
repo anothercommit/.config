2d82eeaeda9b2cfb7da9980b41322b9ccd81a839
anothercommit <lack_of_names@proton.me>
2024-04-02 16:33:02 -0300

    🦎

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -324,9 +324,9 @@ globalkeys = gears.table.join(
 	end, { description = "go back", group = "client" }),
 
 	-- Standard program
-	awful.key({ modkey }, "Return", function()
-		awful.spawn(terminal)
-	end, { description = "open a terminal", group = "run" }),
+	-- awful.key({ modkey }, "Return", function()
+	-- 	awful.spawn(terminal)
+	-- end, { description = "open a terminal", group = "run" }),
 	awful.key({ modkey, "Control" }, "r", awesome.restart, { description = "reload awesome", group = "awesome" }),
 	awful.key({ modkey, "Shift" }, "q", awesome.quit, { description = "quit awesome", group = "awesome" }),
 	awful.key({ modkey }, "l", function()
