5c52aa7f9cea12b0ea90749b9983c1925e773dc7
Your Name <you@example.com>
2024-04-08 09:01:02 -0300

    🦎

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -619,4 +619,4 @@ client.connect_signal("unfocus", function(c)
 	c.border_color = beautiful.border_normal
 end)
 -- }}}
-awful.spawn.with_shell("~/.config/awesome/autorun.sh")
+awful.spawn.with_shell("~/.config/awesome/./autorun.sh")
--- nvim/init.lua
+++ nvim/init.lua
@@ -52,4 +52,6 @@ require('lazy').setup({
 
 if vim.loop.os_uname().sysname == 'Darwin' then
   vim.g.python3_host_prog = '/Library/Frameworks/Python.framework/Versions/3.12/bin/python3'
+else
+  vim.g.python3_host_prog = '/usr/bin/python3'
 end
