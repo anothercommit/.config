9771262dd6aad54680770e025cd482ed97c06b8f
Tu nombre <you@example.com>
2024-08-18 00:32:43 -0300

    domingo, 18 de agosto de 2024, 00:32:43 -03

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,73 +1,49 @@
 return {
   {
-    -- 'projekt0n/github-nvim-theme',
     'folke/tokyonight.nvim',
     'nyoom-engineering/oxocarbon.nvim',
-    'dgox16/oldworld.nvim',
-    'Yazeed1s/oh-lucy.nvim',
     'Yazeed1s/minimal.nvim',
     'olivercederborg/poimandres.nvim',
-    'zootedb0t/citruszest.nvim',
-    'EdenEast/nightfox.nvim',
     'akinsho/horizon.nvim',
-
     'olimorris/onedarkpro.nvim',
     -- :OnedarkproExportToKitty (puede exportar el colorscheme a kitty)
-
-    'ribru17/bamboo.nvim',
-    -- config = function()
-    --   require('bamboo').load()
-    -- end,
+  },
+  {
+    'EdenEast/nightfox.nvim',
   },
   {
     'Mofiqul/dracula.nvim',
     opts = {
-      transparent_bg = true,
+      transparent_bg = false,
     },
-  },
-  {
-    'AlexvZyl/nordic.nvim',
-    -- config = function()
-    --   require('nordic').load()
-    -- end,
   },
   {
     'navarasu/onedark.nvim',
     opts = {
-      style = 'warmer', -- 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'
-      transparent_background = true,
+      style = 'warmer', -- dark, darker, cool, deep, warm, warmer, light
+      transparent_background = false,
     },
-    -- config = function()
-    --   require('onedark').load()
-    -- end,
   },
   {
     'neanias/everforest-nvim',
-    config = function()
-      require('everforest').setup {
-        background = 'hard', -- soft, medium, hard
-        ui_contrast = 'high', -- low, high
-      }
-    end,
-    lazy = false,
-    priority = 1000,
-    init = function()
-      vim.cmd.colorscheme 'evergarden'
-    end,
+    opts = {
+      background = 'hard', -- soft, medium, hard
+      ui_contrast = 'high', -- low, high
+    },
   },
   { 'catppuccin/nvim', name = 'catppuccin' },
   {
     'tiagovla/tokyodark.nvim',
-    opts = {
-      transparent_background = true,
-    },
+    opts = { transparent_background = false },
   },
   {
     'comfysage/evergarden',
     opts = {
-      transparent_background = true,
-      contrast_dark = 'medium', -- 'hard'|'medium'|'soft'
+      transparent_background = false,
+      contrast_dark = 'hard', -- 'hard'|'medium'|'soft'
     },
+    lazy = false,
+    priority = 1000,
   },
   {
     'sainnhe/sonokai',
