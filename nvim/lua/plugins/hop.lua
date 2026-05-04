c047fa288723d73176022be223298d46a903df8e
anothercommit <lack_of_names@proton.me>
2026-05-04 00:29:53 -0300

    urepos: Mon May  4 12:29:53 AM -03 2026

--- nvim/lua/plugins/hop.lua
+++ nvim/lua/plugins/hop.lua
@@ -2,7 +2,7 @@ return {
   'smoka7/hop.nvim',
   version = 'v2',
   keys = {
-    { '<leader><leader>', '<cmd>HopWord<cr>', desc = 'HopWord' },
+    { '<leader>k', '<cmd>HopWord<cr>', desc = 'HopWord' },
     { '<leader>l', '<cmd>HopLineStart<cr>', desc = 'Hop[L]ineStart' },
     { '<leader>p', '<cmd>HopPattern<cr>', desc = 'Hop[P]attern' },
   },
